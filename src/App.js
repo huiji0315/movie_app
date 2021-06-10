469b8d9275c27caedab7f044dc481086de016833
huiji0315 <huiji0315@gmail.com>
2021-06-10 16:27:12 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
 // 이것을 하지 않으면 react가 jsx로 쓰인 이것을 commponent라고 이해하지 못한다.
 
-function Food({ fav }) {
-  // props.fav == {fav}
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture}/>
+  </div>
 } // 동적 데이터가 있는 component
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return <div>
-    <h1>Hello!!</h1>
-    <Food fav="kimchi" />
-    {/* Parent -> Children Component
-    : Food component에 
-    fav라는 이름의 property를 
-    kimchi라는 value로 준 것 */}
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    {/* jsx + props를 이용하여 재사용 
-     component를 대문자로 시작해야 HTML element로 인식가능 */}
+    {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image}/>
+    ))}
   </div>
 }
+// {}로 표현하면 javascript로 인식한다. {} 없으면 그냥 text로 인식한다.
+// dish는 array의 각 요소(object)
+// map은 function을 취해서 그 function을 array의 각 item에 적용한 후,
+// 해당 function이 적용된 array를 반환한다.
 
 export default App;
 // component는 HTML을 반환하는 함수다.
