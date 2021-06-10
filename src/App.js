13071b3d0237b1bb45febcd953373763c25ac57f
huiji0315 <huiji0315@gmail.com>
2021-06-10 16:40:13 +0900

    #2.3 map Recap - key prop & img alt

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
 import React from 'react';
 // 이것을 하지 않으면 react가 jsx로 쓰인 이것을 commponent라고 이해하지 못한다.
 
-function Food({ name, picture }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture}/>
-  </div>
-} // 동적 데이터가 있는 component
-
 const foodILike = [
   {
+    id:1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id:2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id:3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id:4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id:5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
+// react의 모든 element들은 다르게 보일 필요가 있다.
 
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />;
-}
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name}/>
+  </div>
+} // 동적 데이터가 있는 component
 
 function App() {
   return <div>
-    {foodILike.map(renderFood)}
+    {foodILike.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image}/>
+    ))}
   </div>
 }
-// {}로 표현하면 javascript로 인식한다. {} 없으면 그냥 text로 인식한다.
-// dish는 array의 각 요소(object)
-// map은 function을 취해서 그 function을 array의 각 item에 적용한 후,
-// 해당 function이 적용된 array를 반환한다.
+// key prop은 Food로 전달되지 않는다. 
+// Food 자체에서 사용하는 것이 아니라, 기본적으로 react 내부에서 사용하기 위함이다.
+
 
 export default App;
 // component는 HTML을 반환하는 함수다.
